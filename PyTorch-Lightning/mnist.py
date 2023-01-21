04ee90c0ad0d86e62b7d06f33d20ac9f7e211cc2
Eshban Suleman <eshban9492@gmail.com>
2023-01-21 20:24:26 +0500

    Added MLP Class

--- PyTorch-Lightning/mnist.py
+++ PyTorch-Lightning/mnist.py
@@ -19,3 +19,31 @@ import wandb
 from pytorch_lightning.loggers import WandbLogger
 wandb.login()
 
+class LitMLP(pl.LightningModule):
+    def __init__(self, in_dims, n_classes=10,
+                n_layer_1=128, n_layer_2=256, lr=1e-4):
+        super().__init__()
+        self.layer_1 = nn.Linear(np.prod(in_dims), n_layer_1)
+        self.layer_2 = nn.Linear(n_layer_1, n_layer_2)
+        self.layer_3 = nn.Linear(n_layer_2, n_classes)
+
+        self.save_hyperparameters()
+
+        self.train_acc = torchmetrics.Accuracy()
+        self.valid_acc = torchmetrics.Accuracy()
+        self.test_acc = torchmetrics.Accuracy()
+
+    def forward(self, x):
+        batch_size, *dims = x.size()
+        x = x.view(batch_size, -1)
+        x = F.relu(self.layer_1(x))
+        x = F.relu(self.layer_2(x))
+        x = self.layer_3(x)
+        x = F.log_softmax(x, dim=1)
+        return x
+
+    def loss(self, xs, ys):
+        logits = self(xs)
+        loss = F.null_loss(logits, ys)
+        return logits, loss
+
