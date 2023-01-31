24ca6360c74a25cf31a142da3dc59708337bd962
Eshban Suleman <eshban9492@gmail.com>
2023-01-31 20:52:37 +0500

    wandb initialization

--- JAX/cifar10.py
+++ JAX/cifar10.py
@@ -16,3 +16,22 @@ from tqdm.auto import tqdm
 
 import tensorflow as tf
 import tensorflow_datasets as tfds
+
+wandb.init(
+    project="cifar10-jax",
+    entity="jax",
+    job_type="training-loop"
+)
+
+config = wandb.config
+config.seed = 42
+config.batch_size = 64
+config.validation_split = 0.2
+config.pooling = "avg"
+config.learning_rate = 1e-4
+config.epochs = 15
+
+MODULE_DICT = {
+    "avg": nn.avg_pool,
+    "max": nn.max_pool
+}
