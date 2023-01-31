d1c4a5acf78fba5838b525e81cbe570c956245f5
Eshban Suleman <eshban9492@gmail.com>
2023-01-31 20:55:34 +0500

    prepared train, val and test data

--- JAX/cifar10.py
+++ JAX/cifar10.py
@@ -43,3 +43,53 @@ MODULE_DICT = {
     as_supervised=True,
     with_info=True
 )
+
+def normalize_img(image, label):
+  image = tf.cast(image, tf.float32) / 255.
+  return image, label
+
+full_train_set = full_train_set.map(
+    normalize_img,
+    num_parallel_calls=tf.data.AUTOTUNE
+)
+
+num_data = tf.data.experimental.cardinality(
+    full_train_set
+).numpy()
+
+print(f"Total number of data points: {num_data}")
+
+train_dataset = full_train_set.take(
+    num_data * (1 - config.validation_split)
+)
+
+val_dataset = full_train_set.take(
+    num_data * (config.validation_split)
+)
+
+print(f"Number of train examples: {tf.data.experimental.cardinality(train_dataset).numpy()}")
+print(f"Number of validation examples: {tf.data.experimental.cardinality(val_dataset).numpy()}")
+
+train_dataset = train_dataset.cache()
+train_dataset = train_dataset.shuffle(
+    tf.data.experimental.cardinality(train_dataset).numpy()
+)
+
+train_dataset = train_dataset.batch(config.batch_size)
+
+val_dataset = val_dataset.cache()
+val_dataset = val_dataset.shuffle(
+    tf.data.experimental.cardinality(val_dataset).numpy()
+)
+
+val_dataset = val_dataset.batch(config.batch_size)
+
+test_dataset = test_dataset.map(
+    normalize_img,
+    num_parallel_calls=tf.data.AUTOTUNE
+)
+
+print(f"Number of test records: {tf.data.experimental.cardinality(test_dataset).numpy()}")
+
+test_dataset = test_dataset.cache()
+test_dataset = test_dataset.batch(config.batch_size)
