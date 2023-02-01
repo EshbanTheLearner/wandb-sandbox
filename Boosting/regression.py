bce25e752f2f386c52f816024533a2af59e8833f
Eshban Suleman <eshban9492@gmail.com>
2023-02-01 23:24:39 +0500

    Added training code

--- Boosting/regression.py
+++ Boosting/regression.py
@@ -19,3 +19,30 @@ X_test = df_test.drop(0, axis=1)
 # create dataset for lightgbm
 lgb_train = lgb.Dataset(X_train, y_train)
 lgb_eval = lgb.Dataset(X_test, y_test, reference=lgb_train)
+
+# specify your configurations as a dict
+params = {
+    'boosting_type': 'gbdt',
+    'objective': 'regression',
+    'metric': ['rmse', 'l2', 'l1', 'huber'],
+    'num_leaves': 31,
+    'learning_rate': 0.05,
+    'feature_fraction': 0.9,
+    'bagging_fraction': 0.8,
+    'bagging_freq': 5,
+    'verbosity': 0
+}
+
+wandb.init(project='regression-lightgbm', config=params)
+
+gbm = lgb.train(
+    params,
+    lgb_train,
+    num_boost_round=30,
+    valid_sets=lgb_eval,
+    valid_names=('validation'),
+    callbacks=[wandb_callback()],
+    early_stopping_rounds=5
+)
+
+log_summary(gbm, save_model_checkpoint=True)
