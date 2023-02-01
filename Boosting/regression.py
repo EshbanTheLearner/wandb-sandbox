49b3c2758def589fe3b7e4668851e3f57e4863ee
Eshban Suleman <eshban9492@gmail.com>
2023-02-01 23:23:08 +0500

    Added Data Prep Code

--- Boosting/regression.py
+++ Boosting/regression.py
@@ -6,3 +6,16 @@ import wandb
 from wandb.lightgbm import wandb_callback, log_summary
 
 wandb.login()
+
+# load or create your dataset
+df_train = pd.read_csv('regression.train', header=None, sep='\t')
+df_test = pd.read_csv('regression.test', header=None, sep='\t')
+
+y_train = df_train[0]
+y_test = df_test[0]
+X_train = df_train.drop(0, axis=1)
+X_test = df_test.drop(0, axis=1)
+
+# create dataset for lightgbm
+lgb_train = lgb.Dataset(X_train, y_train)
+lgb_eval = lgb.Dataset(X_test, y_test, reference=lgb_train)
