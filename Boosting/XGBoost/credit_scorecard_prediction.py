ae3b5dd5d586d96d91113e41d35e1f8155a63524
Eshban Suleman <eshban9492@gmail.com>
2023-02-02 16:07:56 +0500

    Added preprocessing code

--- Boosting/XGBoost/credit_scorecard_prediction.py
+++ Boosting/XGBoost/credit_scorecard_prediction.py
@@ -41,3 +41,32 @@ dataset_art = run.use_artifact(
 )
 dataset_dir = dataset_art.download(data_dir)
 
+from data_utils import (
+    describe_data_g_targ,
+    one_hot_encode_data,
+    create_feature_interaction_constraints,
+    get_monotonic_constraints,
+    load_training_data,
+    calculate_credit_scores
+)
+from scorecard import generate_scorecard
+
+dataset = pd.read_csv(data_dir/"vehicle_loans_subset.csv")
+
+dataset, p_vars = one_hot_encode_data(dataset, id_vars, targ_var)
+
+processed_data_path = data_dir/"proc_ds.csv"
+dataset.to_csv(processed_data_path, index=False)
+
+processed_ds_art = wandb.Artifact(
+    name="vehicle_defaults_processed",
+    type="processed_dataset",
+    description="One-Hot Encoded Dataset",
+    metadata={
+        "preprocessing_fn": function_to_string(one_hot_encode_data)
+    }
+)
+
+processed_ds_art.add_file(processed_data_path)
+run.log_artifact(processed_ds_art)
+run.finish()
