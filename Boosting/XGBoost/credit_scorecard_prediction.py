e09b6295ac3e635f0f459728dfce9e5581e97a8c
Eshban Suleman <eshban9492@gmail.com>
2023-02-02 15:57:43 +0500

    Initialized wandb and fetched data

--- Boosting/XGBoost/credit_scorecard_prediction.py
+++ Boosting/XGBoost/credit_scorecard_prediction.py
@@ -17,3 +17,27 @@ import xgboost as xgb
 
 import wandb
 wandb.login()
+
+data_dir = Path(".")
+model_dir = Path("models")
+model_dir.mkdir(exist_ok=True)
+
+id_vars = ["UniqueID"]
+targ_var = "loan_default"
+
+def function_to_string(fn):
+    return getsource(detect.code(fn))
+
+run = wandb.init(
+    project="xgboost-credit-scorecard",
+    config={
+        "wandb_nb": "wandb_credit_soc"
+    }
+)
+
+dataset_art = run.use_artifact(
+    "morgan/credit_scorecard/vehicle_loan_defaults:latest", 
+    type="dataset"
+)
+dataset_dir = dataset_art.download(data_dir)
+
