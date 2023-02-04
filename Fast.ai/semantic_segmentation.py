7ac00b5b90de8f753d11b37990ef87781cf8ef50
Eshban Suleman <eshban9492@gmail.com>
2023-02-04 13:32:21 +0500

    setup fastai pipeline

--- Fast.ai/semantic_segmentation.py
+++ Fast.ai/semantic_segmentation.py
@@ -4,3 +4,26 @@ from fastai.callback.wandb import *
 import wandb
 wandb.login()
 
+path = untar_data(URLs.PETS)/"images"
+SEED = 42
+
+def is_cat(x):
+    return x[0].isupper()
+
+dls = ImageDataLoaders.from_name_func(
+    path,
+    get_image_files(path),
+    valid_pct=0.2,
+    seed=SEED,
+    bs=32,
+    label_func=is_cat,
+    item_tfms=Resize(128)
+)
+
+learn = vision_learner(
+    dls, 
+    "convert_tiny",
+    metrics=error_rate
+)
+learn.fine_tune(1)
+
