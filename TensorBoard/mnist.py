6017a352be9439b82d9ada6b3e5719262458e18b
Eshban Suleman <eshban9492@gmail.com>
2023-02-08 12:35:25 +0500

    loaded mnist data

--- TensorBoard/mnist.py
+++ TensorBoard/mnist.py
@@ -8,3 +8,9 @@ wandb.init(
     project="MNIST-TF",
     sync_tensorboard=True
 )
+
+mnist = tf.keras.datasts.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train, x_test = x_train / 255.0, x_test / 255.0
