406aa0b0b3388d79e7e7d3d132f54e6a0fe3d074
Eshban Suleman <eshban9492@gmail.com>
2023-02-12 14:09:13 +0500

    Defined NN Architecture

--- PyTorch-Profiler/mnist.py
+++ PyTorch-Profiler/mnist.py
@@ -9,3 +9,44 @@ from torchvision import datasets, transforms
 
 from torch.profiler import tensorboard_trace_handler
 import wandb
+
+torchvision.datasets.MNIST.mirrors = [mirror for mirror in torchvision.datasets.MNIST.mirrors if not mirror.startswith("http://yann.lecun.com")]
+
+wandb.login()
+
+OPTIMIZERS = {
+    "Adadelta": optim.Adadelta,
+    "Adagrad": optim.Adagrad,
+    "SGD": optim.SGD
+}
+
+class Net(pl.LightningModule):
+    def __init__(self, optimizer="Adadelta"):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
+        self.optimizer = self.set_optimizer(optimizer)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+    
+    def set_optimizer(self, optimizer):
+        return OPTIMIZERS[optimizer]
+    
+
