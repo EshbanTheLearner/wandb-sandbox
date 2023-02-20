ebb41d9d663a09bb7dfdb855ab4a0dc3d642c965
Eshban Suleman <eshban9492@gmail.com>
2023-02-21 01:35:52 +0500

    Added train and test data

--- PyTorch/cifar-dropout-test.py
+++ PyTorch/cifar-dropout-test.py
@@ -10,3 +10,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 import wandb
+wandb.login()
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+
+BATCH_SIZE = 32
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+trainset = torchvision.datasets.CIFAR10(root="../data", train=True, download=True, transforms=transform)
+testset = torchvision.datasets.CIFAR10(root="../data", train=False, download=True, transform=transform)
+
+trainloader = DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
+testloader = DataLoader(testset, batch_size=BATCH_SIZE, shuffle=False, num_workers=2)
+
+CLASS_NAMES = (
+    "plane", "car", "bird", "cat", "deer",
+    "dog", "frog", "horse", "ship",  "truck"
+)
