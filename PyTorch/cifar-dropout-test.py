e60d0760674c334f23538288b4503d5773b9d730
Eshban Suleman <eshban9492@gmail.com>
2023-02-21 22:41:20 +0500

    Defined NN Architecture

--- PyTorch/cifar-dropout-test.py
+++ PyTorch/cifar-dropout-test.py
@@ -82,3 +82,37 @@ def test(model, device, test_loader, criterion, classes):
         "Test Accuracy": acc
     })
 
+class Net(nn.Module):
+  def __init__(self, input_shape=(3,32,32)):
+    super(Net, self).__init__()
+    
+    self.conv1 = nn.Conv2d(3, 32, 3)
+    self.conv2 = nn.Conv2d(32, 64, 3)
+    self.conv3 = nn.Conv2d(64, 128, 3)
+    
+    self.pool = nn.MaxPool2d(2,2)
+
+    n_size = self._get_conv_output(input_shape)
+    
+    self.fc1 = nn.Linear(n_size, 512)
+    self.fc2 = nn.Linear(512, 10)
+
+  def _get_conv_output(self, shape):
+    batch_size = 1
+    input = torch.autograd.Variable(torch.rand(batch_size, *shape))
+    output_feat = self._forward_features(input)
+    n_size = output_feat.data.view(batch_size, -1).size(1)
+    return n_size
+
+  def _forward_features(self, x):
+    x = self.pool(F.relu(self.conv1(x)))
+    x = self.pool(F.relu(self.conv2(x)))
+    x = self.pool(F.relu(self.conv3(x)))
+    return x
+      
+  def forward(self, x):
+    x = self._forward_features(x)
+    x = x.view(x.size(0), -1)
+    x = F.relu(self.fc1(x))
+    x = self.fc2(x)
+    return x
